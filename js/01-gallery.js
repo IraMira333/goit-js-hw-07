3d5a370a94f423119f0fc4ce4c0f04b1ff704d5a
IraMira333 <irinat333@gmail.com>
2023-04-22 16:16:27 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+//<li><img src="" alt=""></li>
+
+const containerGalleryImages = document.querySelector(".gallery");
+const imagesMarkup = creatFotoCardsMurkup(galleryItems);
+
+containerGalleryImages.insertAdjacentHTML("beforeend", imagesMarkup);
+
+containerGalleryImages.addEventListener("click", onImageClick);
+
+function creatFotoCardsMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+      `;
+    })
+    .join("");
+}
+//console.log(creatFotoCardsMurkup(galleryItems));
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const bigImage = evt.target.dataset.source;
+  evt.target.src = bigImage;
+  console.log(evt.target.src);
+}
